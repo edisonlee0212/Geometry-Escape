212550c78d9919ede749e87d91da2a85b9f7e78e
Edison Li <li2343@purdue.edu>
2019-09-23 13:02:59 -0400

    Suggestions for XYF

--- Geometry-Escape/Assets/MainScripts/ECS/UISystem.cs
+++ Geometry-Escape/Assets/MainScripts/ECS/UISystem.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class UISystem : MonoBehaviour
 {
+    //This [SerializeField] will add a option in the inspector. Click on the UISystem in the scene to see the results.
+    [SerializeField]
+    private GameObject m_Hit_300;
+    [SerializeField]
+    private GameObject m_Miss;
+
     public static GameObject hit_300;
     public static GameObject miss;
     // Start is called before the first frame update
     void Start()
     {
-        hit_300 = GameObject.Find("hit_300");
-        miss = GameObject.Find("Miss");
+        /*Don't use GameObject.Find, its not safe and its slow. Consider multiple "miss"
+         * Consider you renamed "Miss"
+         * Consider you have too many game objects
+         */
+        //hit_300 = GameObject.Find("hit_300");
+        //miss = GameObject.Find("Miss");
+        hit_300 = m_Hit_300;
+        miss = m_Miss;
         Debug.Log("init");
         HideHit_300();
         HideMiss();
