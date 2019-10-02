048cddbd2b5d8cb8b71d33e0c2d2fcdf530650ed
Ray <chen2733@purdue.edu>
2019-10-02 07:30:15 -0400

    Initial Basic Menu

--- Geometry-Escape/Assets/MainScripts/Mono/CentralSystemControllers/CentralSystemMainMenuController.cs
+++ Geometry-Escape/Assets/MainScripts/Mono/CentralSystemControllers/CentralSystemMainMenuController.cs
@@ -3,19 +3,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace GeometryEscape {
-    public class CentralSystemMainMenuController : MonoBehaviour
-    {
+    public class CentralSystemMainMenuController : MonoBehaviour {
         public CentralSystem m_CentralSystem;
         // Start is called before the first frame update
-        void Start()
-        {
+        void Start() {
             m_CentralSystem = World.Active.GetOrCreateSystem<CentralSystem>();
         }
 
-        public void OnStartButtonPressed()
-        {
+        public void OnStartButtonPressed() {
 
             SceneManager.LoadScene("In-Game", LoadSceneMode.Single);
+        }
+
+        public void OnExitButtonPressed() {
+            Application.Quit();
         }
     }
 }
